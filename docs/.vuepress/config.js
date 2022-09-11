e2cf51455a5923cdc95f5643a0413f97836d2feb
smori <shinichiro.mori.1983@gmail.com>
2022-09-11 13:02:42 +0900

    Remove pages except README.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,52 +9,52 @@ module.exports = {
       { text: 'npm', link: 'https://www.npmjs.com/package/vuepress-plugin-frontmatter-update-info' },
     ],
     sidebar: [
-      {
-        title: 'Main',
-        collapsable: false,
-        sidebarDepth: 0,
-        children: [
-          '/debug.md',
-          '/update_info_list.md',
-          '/update_info_table.md',
-        ],
-      },
-      {
-        title: 'Pages: basic',
-        collapsable: false,
-        sidebarDepth: 0,
-        children: [
-          '/pages/basic_01.md',
-          '/pages/basic_02.md',
-          '/pages/basic_03.md',
-          '/pages/basic_04.md',
-          '/pages/basic_05.md',
-        ],
-      },
-      {
-        title: 'Pages: custom tag',
-        collapsable: false,
-        sidebarDepth: 0,
-        children: [
-          '/pages/custom_tag_01.md',
-          '/pages/custom_tag_02.md',
-          '/pages/custom_tag_03.md',
-        ],
-      },
-      {
-        title: 'Pages: invalid',
-        collapsable: false,
-        sidebarDepth: 0,
-        children: [
-          '/pages/invalid_01.md',
-          '/pages/invalid_02.md',
-          '/pages/invalid_03.md',
-          '/pages/invalid_04.md',
-          '/pages/invalid_05.md',
-          '/pages/invalid_06.md',
-          '/pages/invalid_07.md',
-        ],
-      },
+      //{
+      //  title: 'Main',
+      //  collapsable: false,
+      //  sidebarDepth: 0,
+      //  children: [
+      //    '/debug.md',
+      //    '/update_info_list.md',
+      //    '/update_info_table.md',
+      //  ],
+      //},
+      //{
+      //  title: 'Pages: basic',
+      //  collapsable: false,
+      //  sidebarDepth: 0,
+      //  children: [
+      //    '/pages/basic_01.md',
+      //    '/pages/basic_02.md',
+      //    '/pages/basic_03.md',
+      //    '/pages/basic_04.md',
+      //    '/pages/basic_05.md',
+      //  ],
+      //},
+      //{
+      //  title: 'Pages: custom tag',
+      //  collapsable: false,
+      //  sidebarDepth: 0,
+      //  children: [
+      //    '/pages/custom_tag_01.md',
+      //    '/pages/custom_tag_02.md',
+      //    '/pages/custom_tag_03.md',
+      //  ],
+      //},
+      //{
+      //  title: 'Pages: invalid',
+      //  collapsable: false,
+      //  sidebarDepth: 0,
+      //  children: [
+      //    '/pages/invalid_01.md',
+      //    '/pages/invalid_02.md',
+      //    '/pages/invalid_03.md',
+      //    '/pages/invalid_04.md',
+      //    '/pages/invalid_05.md',
+      //    '/pages/invalid_06.md',
+      //    '/pages/invalid_07.md',
+      //  ],
+      //},
     ],
   },
 
