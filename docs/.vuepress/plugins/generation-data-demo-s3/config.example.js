aef5786e64d82022e827d36700df62d6eb9100c3
smori <shinichiro.mori.1983@gmail.com>
2022-09-17 03:08:20 +0900

    [s3] example file for local config #1

---
+++ docs/.vuepress/plugins/generation-data-demo-s3/config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  clientConfig: {
+    region: 'ap-northeast-1',
+    credentials: {
+      accessKeyId: 'XXXXXXXXXXXXXXXXXXXX',
+      secretAccessKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    },
+  },
+  bucket: 'XXXXXXXXXX',
+};
