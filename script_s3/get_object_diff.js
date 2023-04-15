95fb4b9093c954d41ef3bd714e832283fd1424ba
smori <shinichiro.mori.1983@gmail.com>
2023-04-16 00:17:31 +0900

    script_s3/s3.js

--- script_s3/get_object_diff.js
+++ script_s3/get_object_diff.js
@@ -5,7 +5,6 @@ const {
 } = require('./config.local');
 const {
   S3Client,
-  GetObjectCommand,
 } = require('@aws-sdk/client-s3');
 const {
   difference,
@@ -14,42 +13,13 @@ const {
   orderBy,
 } = require('lodash');
 const hash = require('hash-sum');
-
-/**
- * @param {S3Client} client
- * @param {string} bucket
- * @param {string} key
- * @return {Promise<string>}
- *
- * Thanks:
- * https://stackoverflow.com/questions/36942442/how-to-get-response-from-s3-getobject-in-node-js#36944450
- */
-const getObject = async (client, bucket, key) => {
-  const getCommand = new GetObjectCommand({
-    Bucket: bucket,
-    Key: key,
-  });
-
-  return new Promise(async (resolve, reject) => {
-    try {
-      const response = await client.send(getCommand);
-
-      let responseDataChunks = [];
-
-      response.Body.once('error', (err) => reject(err));
-      response.Body.on('data', (chunk) => responseDataChunks.push(chunk));
-      response.Body.once('end', () => resolve(responseDataChunks.join('')));
-    } catch (err) {
-      return reject(err);
-    }
-  });
-};
+const s3 = require('./s3');
 
 (async () => {
   const client = new S3Client(clientConfig);
 
   try {
-    const json = await getObject(client, targetBucket, objectKey);
+    const json = await s3.getObject(client, targetBucket, objectKey);
     console.log(JSON.stringify(getDiff(json), null, 2));
   } catch (e) {
     console.log(e);
