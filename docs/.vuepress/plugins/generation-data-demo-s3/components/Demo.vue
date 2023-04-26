55a9df2baf8ab6a9f6b5d9403a21bd6ef4a9893d
smori <shinichiro.mori.1983@gmail.com>
2023-04-27 01:19:10 +0900

    generation-data-demo-s3 - reimplement Demo.vue using generation-util

--- docs/.vuepress/plugins/generation-data-demo-s3/components/Demo.vue
+++ docs/.vuepress/plugins/generation-data-demo-s3/components/Demo.vue
@@ -1,20 +1,40 @@
 <template>
   <div>
-    <h3>Page list (update info newly added)</h3>
-    <pre class="json">{{ newPaths }}</pre>
-
-    <h3>Page list (update info already added)</h3>
-    <pre class="json">{{ existingPaths }}</pre>
-
-    <h3>List of new update info</h3>
+    <h3>Diff of current and previous generations (based on date)</h3>
+    <p>
+      Using
+      <a
+        target="_blank"
+        href="https://github.com/smori1983/vuepress-plugin-frontmatter-update-info/blob/master/src/generation-util/diff-style-date.js"
+      >
+        DiffStyleDate<OutboundLink />
+      </a>
+    </p>
+    <p>Records are extracted if:</p>
     <ul>
-      <li>New updates: added them all to list</li>
-      <li>Existing updates: added to list if descriptions edited (based on <code>recordsHash</code>)</li>
-      <li>Empty list means there is no new update info between previous and current deployments.</li>
+      <li>The date is new one in current generation.</li>
     </ul>
-    <pre class="json">{{ targetPages }}</pre>
+    <pre class="json">{{ diffDate }}</pre>
+
+    <h3>Diff of current and previous generations (based on date and description)</h3>
+    <p>
+      Using
+      <a
+        target="_blank"
+        href="https://github.com/smori1983/vuepress-plugin-frontmatter-update-info/blob/master/src/generation-util/diff-style-date-description.js"
+      >
+        DiffStyleDateDescription<OutboundLink />
+      </a>
+    </p>
+    <p>Records are extracted if:</p>
+    <ul>
+      <li>The date is new one in current generation, or</li>
+      <li>The date is not new but the description was modified.</li>
+    </ul>
+    <pre class="json">{{ diffDateDescription }}</pre>
 
     <h3>Update info of current and previous generations</h3>
+    <p>Stored on Amazon S3.</p>
     <ul>
       <li><code>generation_0</code>: current</li>
       <li><code>generation_1</code>: previous</li>
@@ -24,52 +44,31 @@
 </template>
 
 <script>
+import OutboundLink from '@vuepress/core/lib/client/components/OutboundLink.vue';
 import data from '@dynamic/generation-data-demo-s3/data';
+import Generation from 'vuepress-plugin-frontmatter-update-info/src/generation-util/generation';
+import DiffStyleDate from 'vuepress-plugin-frontmatter-update-info/src/generation-util/diff-style-date';
+import DiffStyleDateDescription from 'vuepress-plugin-frontmatter-update-info/src/generation-util/diff-style-date-description';
 
 export default {
+  components: {
+    OutboundLink,
+  },
+
   data() {
     return {
       data: data,
-      newPaths: [],
-      existingPaths: [],
-      targetPages: [],
+      diffDate: [],
+      diffDateDescription: [],
     };
   },
 
   mounted() {
-    const generation0Paths = data.generation_0.map(page => page.path);
-    const generation1Paths = data.generation_1.map(page => page.path);
+    const generation0 = new Generation(data.generation_0);
+    const generation1 = new Generation(data.generation_1);
 
-    const targetPages = [];
-
-    this.newPaths = generation0Paths.filter((path) => !generation1Paths.includes(path)).sort();
-    this.existingPaths = generation0Paths.filter((path) => generation1Paths.includes(path)).sort();
-
-    this.newPaths.forEach((path) => {
-      targetPages.push(this.findPageByPath(data.generation_0, path));
-    })
-
-    this.existingPaths.forEach((path) => {
-      const generation0Page = this.findPageByPath(data.generation_0, path);
-      const generation1Page = this.findPageByPath(data.generation_1, path);
-
-      if (generation0Page.recordsHash !== generation1Page.recordsHash) {
-        targetPages.push(generation0Page);
-      }
-    });
-
-    this.targetPages = targetPages;
-  },
-
-  methods: {
-    /**
-     * @param pages
-     * @param path
-     * @return {Object}
-     */
-    findPageByPath(pages, path) {
-      return pages.find(page => page.path === path);
-    },
+    this.diffDate = new DiffStyleDate().get(generation0, generation1);
+    this.diffDateDescription = new DiffStyleDateDescription().get(generation0, generation1);
   },
 };
 </script>
